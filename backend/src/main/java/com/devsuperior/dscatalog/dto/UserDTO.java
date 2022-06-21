eeb97df947d7bccf037b8bd2d02236da44b3aba5
Samuel M Santos <samuelmacedo2011@gmail.com>
2022-06-21 11:55:46 -0300

    Inicio de validação básica com Bean Validation em User e Products

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
@@ -4,7 +4,8 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.FetchType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 import com.devsuperior.dscatalog.entities.User;
 
@@ -12,8 +13,12 @@ public class UserDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+	
+	@NotBlank(message = "Campo Obrigatório")
 	private String firstName;
 	private String lastName;
+	
+	@Email(message = "Favor entrar um e-mail válido")
 	private String email;
 
 	Set<RoleDTO> roles = new HashSet<>();

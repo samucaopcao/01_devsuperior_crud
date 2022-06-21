f7d4a3b13b49d0e6caddaade8a99e53be7258cf7
Samuel M Santos <samuelmacedo2011@gmail.com>
2022-06-21 13:04:59 -0300

    Melhora na Response dos @Validation

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,10 +18,11 @@ import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-	//Será necessária essa anotation com o parametro sendo a classe da nossa excessão
-	//nesse caso a classe que criamos EntityNotFoundException
-	@ExceptionHandler(ResourceNotFoundException.class)	
-	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
+	// Será necessária essa anotation com o parametro sendo a classe da nossa
+	// excessão
+	// nesse caso a classe que criamos EntityNotFoundException
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError();
 		err.setTimeStamp(Instant.now());
@@ -29,8 +32,9 @@ public class ResourceExceptionHandler {
 		err.setPath(request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
-	@ExceptionHandler(DataBaseException.class)	
-	public ResponseEntity<StandardError> database(DataBaseException e, HttpServletRequest request){
+
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> database(DataBaseException e, HttpServletRequest request) {
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError();
 		err.setTimeStamp(Instant.now());
@@ -39,5 +43,27 @@ public class ResourceExceptionHandler {
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
 		return ResponseEntity.status(status.value()).body(err);
+
+	}
+
+	// Entidade não foi possível ser processada.Ex.: Preço de produto digitado sendo
+	// valor negativo.
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		ValidationError err = new ValidationError();
+		err.setTimeStamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Validation exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+
+		// Pegara os erros específicos que aconteceram na validação @Valid
+		// e os campos que deram erro
+		for (FieldError f : e.getBindingResult().getFieldErrors()) {
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
+
+		return ResponseEntity.status(status).body(err);
 	}
 }
